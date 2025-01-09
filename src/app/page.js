fcc14ad501b2b6d37b3842922a83895d1158e3c6
Ragib Barket <ragibrahman86317@gmail.com>
2025-01-09 15:29:38 +0600

    .

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Features from '@/components/features/features'
 import Navbar from '@/components/header/Navbar'
 import Hero from '@/components/hero/Hero'
 import Overview from '@/components/overviewCard/Overview'
-import ThemeToggle from '@/components/ThemeToggle'
 import Footer from '@/components/footer/Footer'
 
 export default function Home() {
