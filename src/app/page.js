d3a625917c17bc3bea962a3493a2e255b9f72988
Ragib Barket <ragibrahman86317@gmail.com>
2025-01-09 15:33:01 +0600

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import Article from '@/components/article/Article'
-import Features from '@/components/features/features'
 import Navbar from '@/components/header/Navbar'
 import Hero from '@/components/hero/Hero'
 import Overview from '@/components/overviewCard/Overview'
 import Footer from '@/components/footer/Footer'
+import Features from '@/components/features/Features'
 
 export default function Home() {
   return (
